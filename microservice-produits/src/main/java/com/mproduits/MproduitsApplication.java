e1db26995d556a84c67b43ac29ab0b5c7ffe13eb
s-deb <s-deb@enageo.com>
2020-05-18 12:30:17 +0100

    eureka config

--- microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
+++ microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
@@ -2,9 +2,11 @@ package com.mproduits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 @EnableDiscoveryClient
 public class MproduitsApplication {
 
