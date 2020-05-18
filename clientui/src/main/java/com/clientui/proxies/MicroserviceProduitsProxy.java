ae7c364647ae522f457c3572f37e269b30c0c18c
s-deb <s-deb@enageo.com>
2020-05-18 14:41:14 +0100

    Zuul secu

--- clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
@@ -7,16 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.Optional;
 
-@FeignClient(name = "microservice-produits")
+@FeignClient(name = "zuul-server")
 @RibbonClient(name = "microservice-produits")
 public interface MicroserviceProduitsProxy {
 
-@GetMapping(value = "/Produits")
-List<ProductBean> listeDesProduits();
+    @GetMapping(value = "/microservice-produits/Produits")
+    List<ProductBean> listeDesProduits();
 
-@GetMapping( value = "/Produits/{id}")
-ProductBean recupererUnProduit(@PathVariable("id") int id);
+    /*
+     * Notez ici la notation @PathVariable("id") qui est différente de celle qu'on utlise dans le contrôleur
+     **/
+    @GetMapping( value = "/microservice-produits/Produits/{id}")
+    ProductBean recupererUnProduit(@PathVariable("id") int id);
+
+
 
 }
